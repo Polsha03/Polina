10e4a28416a1eb77fc577c9f82c3ca7dfc2d9961
Polsha03 <106655505+Polsha03@users.noreply.github.com>
2026-05-12 14:30:27 +0500

    Add files via upload

---
+++ js gone1.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://github.com/Polsha03/Popa-/blob/main/https___images.genius.com_628f42b40c60b8fa362025e06714338a.1000x1000x1%201.png",
+
+  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_73f2a4b6c407fb809c75833221b20dd6.1000x1000x1%201.png",
+
+  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_fd336f065713e6b2b3eb42c19ede6d19.1000x1000x1%201.png",
+
+  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_0829d101b234aeb599ac431ac5fda31c.1000x1000x1%201.png",
+
+  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_4e7c57937aa223f68a8eeb3eb7cb0be3.1000x1000x1%201.png",
+
+  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_b994c9e5ebb56c793d23b6d5954a935c.1000x1000x1%201.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "D0WN T0 M00N",
+
+  "BOYS DON’T CRY",
+
+  "SUPERCHUITS",
+
+  "SINGLE PSYCHIC ATTACK",
+
+  "VOODOO CHILD",
+
+  "FLUDDALITY"
+
+];
