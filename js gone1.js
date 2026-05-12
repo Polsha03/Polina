ac0537db328ac41591cbf32a8b8af7581ed810e3
Polsha03 <106655505+Polsha03@users.noreply.github.com>
2026-05-12 17:20:17 +0500

    Add files via upload

--- js gone1.js
+++ js gone1.js
@@ -1,16 +1,16 @@
 const IMAGE_SRCS = [
 
-  "https://github.com/Polsha03/Popa-/blob/main/https___images.genius.com_628f42b40c60b8fa362025e06714338a.1000x1000x1%201.png",
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___1mages.genius.com_628f42b40c60b8fa362025e06714338a.1000x1000x1%201.png",
 
-  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_73f2a4b6c407fb809c75833221b20dd6.1000x1000x1%201.png",
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___2images.genius.com_73f2a4b6c407fb809c75833221b20dd6.1000x1000x1%201.png",
 
-  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_fd336f065713e6b2b3eb42c19ede6d19.1000x1000x1%201.png",
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___i3mages.genius.com_fd336f065713e6b2b3eb42c19ede6d19.1000x1000x1%201.png",
 
-  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_0829d101b234aeb599ac431ac5fda31c.1000x1000x1%201.png",
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___4images.genius.com_0829d101b234aeb599ac431ac5fda31c.1000x1000x1%201.png",
 
-  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_4e7c57937aa223f68a8eeb3eb7cb0be3.1000x1000x1%201.png",
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___5images.genius.com_4e7c57937aa223f68a8eeb3eb7cb0be3.1000x1000x1%201.png",
 
-  "https://raw.githubusercontent.com/Polsha03/Popa-/refs/heads/main/https___images.genius.com_b994c9e5ebb56c793d23b6d5954a935c.1000x1000x1%201.png"
+  "https://raw.githubusercontent.com/Polsha03/Polina/refs/heads/main/https___6images.genius.com_b994c9e5ebb56c793d23b6d5954a935c.1000x1000x1%201.png"
 
 ];
 const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
